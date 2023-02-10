5e22b123511115656ad2aac6b4db9650b88ae2f8
jonathan cardenas <joercamu@gmail.com>
2023-02-10 03:03:44 -0500

    add coments to pokemons Component

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -4,16 +4,26 @@ import { Pokemon } from './Pokemon';
 
 
 
+// Componente que renderiza una lista de Pokemon's
 export function Pokemons () {
   
-  const { pokemons, loading, limit, offset, handleClick, hasPokemons, pokemonsClear } = usePokemons();
-
+  // Desestructuración de los valores devueltos por el hook usePokemons
+  const { 
+    pokemons, // Un arreglo de objetos, cada objeto representa un Pokemon
+    loading, // Un booleano que indica si los Pokemons están siendo cargados o no
+    limit, // Un número que representa el límite de Pokemons a mostrar por página
+    offset, // Un número que representa la cantidad de Pokemons a saltarse en la lista
+    handleClick, // Una función que maneja el evento de click en la paginación
+    hasPokemons, // Un booleano que indica si hay Pokemons disponibles para mostrar
+    pokemonsClear // Una función que limpia los Pokemons mostrados en pantalla
+  } = usePokemons()
   return (
     <div  className='container'>
       {
         loading 
         ? <p> Cargando...</p>
         :
+        // Si hay Pokemons disponibles, se renderiza una lista
         <ul className='pokemons'>
           { hasPokemons && 
           pokemons.map((pokemon, index) => (
